ddb0dc32b49ab5513b8afbb3ca782571498432cd
unknown <pc24@csitlab.local>
2026-08-28 11:19:52 +0800

    Server Activity

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const app = require('express');
 
 //server set up
 const server = app();
-server.listen(1286, () => {
-    console.log("Server is running...");
+const PORT = 1286;
+const HOSTNAME = '0.0.0.0'
+server.listen(POST, HOSTNAME, () => {
+    console.log('Server is running in ${HOSTNAME}:${PORT}');
 });
