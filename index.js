f4406873086ced67d14a7caef86d96b68fa5803f
unknown <pc24@csitlab.local>
2026-08-28 11:35:03 +0800

    feat(server): initialize Express server listening on port 1286

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = require('express');
 //server set up
 const server = app();
 const PORT = 1286;
-const HOSTNAME = '0.0.0.0'
-server.listen(POST, HOSTNAME, () => {
-    console.log('Server is running in ${HOSTNAME}:${PORT}');
+const HOSTNAME = '0.0.0.0';
+server.listen(PORT, HOSTNAME, () => {
+    console.log(`Server is running in ${HOSTNAME}:${PORT}`);
 });
